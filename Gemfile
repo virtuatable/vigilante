50ce079a16f817306fae0b7db5028180afa2e99f
babausse <courtois.vincent@outlook.com>
2019-08-06 12:45:22 -0700

    Implement the basic logic of the vigilante by querying each service

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ group :test, :development, :production do
   gem 'faraday', '0.15.2'
   gem 'mongoid', '7.0.1'
   gem 'arkaan' , '1.6.0'
+  gem 'dotenv' , '2.7.2'
 end
--- watcher.rb
+++ watcher.rb
@@ -1,6 +1,30 @@
-puts "<!DOCTYPE html>"
-puts "<html>"
-puts "  <body>"
-puts "    <h1>Test</h1>"
-puts "  </body>"
-puts "</html>"
+require 'arkaan'
+require 'faraday'
+
+Dotenv.load
+Mongoid.load!('config/mongoid.yml', ENV['RACK_ENV'])
+
+results = {}
+
+logger = Logger.new $stderr
+logger.level = Logger::ERROR
+
+vigilante = Arkaan::Monitoring::Vigilante.first_or_create(token: ENV['VIGILANTE_TOKEN'])
+
+Arkaan::Monitoring::Service.each do |service|
+  puts service.key
+  tmp_results = {}
+  path = "#{service.path}#{service.diagnostic}"
+  service.instances.each do |instance|
+    connection = Faraday.new(url: instance.url) do |faraday|
+      faraday.response :logger, logger
+    end
+    tmp_results[instance.id] = {
+      url: instance.url,
+      health: connection.get(path).status == 200 ? 'ok' : 'ko'
+    }
+  end
+  results[service.key] = tmp_results
+end
+
+puts results.to_json
